cc6ae2c351136be21c1208b472b23bcd980cc64c
DENÍLSON MARCIO DOS SANTOS <48038716+DenilsonMarcio@users.noreply.github.com>
2020-02-15 02:37:01 -0300

    create aplication web combustometro

---
+++ script.js
@@ -0,0 +1,14 @@
+/*function analiza(gasolina,etanol) {
+
+    var gasolina = document.querySelector('.gsl').value;
+    var etanol = document.querySelector('.acl').value;
+    var calc = etanol / gasolina;
+
+    console.log(gasolina);
+    console.log(etanol);
+    console.log(calc);
+    alert(calc);
+}
+TODO implementar o Jquery / Javascript
+*/
+
